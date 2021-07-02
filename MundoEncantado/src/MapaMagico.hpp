d73d687adf519fa0d93c7406e42a3a4285b53c94
Jean Carlo Quesada <jean.quesadaabarca@ucr.ac.cr>
2021-07-02 16:59:47 -0600

    Mapa Magico implementado, no se ha probado aun

--- MundoEncantado/src/MapaMagico.hpp
+++ MundoEncantado/src/MapaMagico.hpp
@@ -1,17 +1,24 @@
 #ifndef MAPAMAGICO_H_
 #define MAPAMAGICO_H_
+#define SALTO_LINEA 'n'
 #include <vector>
+#include <string>
 
 
 class MapaMagico{
     private:
-
     public:
+    int numeroIteraciones;
+    int numeroActual;
+    std::string miNombre; 
     std::vector<std::vector<char>> mapa;
+    std::vector<std::string> resultados;
     MapaMagico();
     MapaMagico(int f, int c);
     void setTamanioMapa(int f, int c);
     void rellenarCaracter(char caracter);
-    void printMe();
+    void procesarMapaActual();
+    friend std::ostream& operator << (std::ostream& ostream,
+                                    const MapaMagico& mapaMagico);
 };
 #endif  // MAPAMAGICO_H_
