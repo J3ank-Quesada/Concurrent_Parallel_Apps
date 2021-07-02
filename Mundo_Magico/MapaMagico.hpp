ad1e44b7b4ea65a47f69fefdcb5694e69e88fe0b
Jean Carlo Quesada <jean.quesadaabarca@ucr.ac.cr>
2021-07-02 10:25:07 -0600

    Commit inicial del proyecto, creacion del mapa

---
+++ Mundo_Magico/MapaMagico.hpp
@@ -0,0 +1,17 @@
+#ifndef MAPAMAGICO_H_
+#define MAPAMAGICO_H_
+#include <vector>
+
+
+class MapaMagico{
+    private:
+
+    public:
+    int numeroF;
+    int numeroC;
+    MapaMagico();
+    std::vector<std::vector<char>> mapa;
+
+
+}
+#endif  // MAPAMAGICO_H_
